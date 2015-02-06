24a701a5d66194f37b3be9704310bec16da253f6
TekuConcept <TekuConcept@ymail.com>
2015-02-06 11:55:29 -0700

    Create CMPR.cpp

---
+++ API/drivers/PowerModule/CMPR.cpp
@@ -0,0 +1,72 @@
+/*
+ * PowerModule.cpp
+ *
+ *  Created on: Feb 6, 2015
+ *      Author: TekuConcept
+ */
+
+#include "CMPR.h"
+
+PowerModule::PowerModule(uint _bus_):
+	bus(_bus_)
+{
+}
+PowerModule::~PowerModule() {
+	// TODO Auto-generated destructor stub
+}
+
+
+
+
+void PowerModule::enableESC(uint8_t esc) {
+	uint8_t dat = bus.read(CMPR_ADDR, CMPR_THST);
+
+	dat |= 1 << esc;
+
+	bus.write(CMPR_ADDR, CMPR_THST, dat);
+}
+void PowerModule::disableESC(uint8_t esc) {
+	uint8_t dat = bus.read(CMPR_ADDR, CMPR_THST);
+
+	dat &= ~(1 << esc);
+
+	bus.write(CMPR_ADDR, CMPR_THST, dat);
+}
+
+
+
+
+void PowerModule::burstEnableESC() {
+	bus.write(CMPR_ADDR, CMPR_THST, 0xFF);
+}
+void PowerModule::burstDisableESC() {
+	bus.write(CMPR_ADDR, CMPR_THST, 0x00);
+}
+
+
+
+
+uint16_t PowerModule::getAmperageA() {
+	uint16_t v = 0;
+	v |= bus.read(CMPR_ADDR, CMPR_A_HI) << 8;
+	v |= bus.read(CMPR_ADDR, CMPR_A_LO);
+	return v;
+}
+uint16_t PowerModule::getAmperageB() {
+	uint16_t v = 0;
+	v |= bus.read(CMPR_ADDR, CMPR_B_HI) << 8;
+	v |= bus.read(CMPR_ADDR, CMPR_B_LO);
+	return v;
+}
+uint16_t PowerModule::getAmperageC() {
+	uint16_t v = 0;
+	v |= bus.read(CMPR_ADDR, CMPR_C_HI) << 8;
+	v |= bus.read(CMPR_ADDR, CMPR_C_LO);
+	return v;
+}
+uint16_t PowerModule::getAmperageD() {
+	uint16_t v = 0;
+	v |= bus.read(CMPR_ADDR, CMPR_D_HI) << 8;
+	v |= bus.read(CMPR_ADDR, CMPR_D_LO);
+	return v;
+}
