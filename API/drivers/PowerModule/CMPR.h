cff873bbf69294f64fe79c0996144699f498a7b7
TekuConcept <TekuConcept@ymail.com>
2015-02-06 11:55:10 -0700

    Create CMPR.h

---
+++ API/drivers/PowerModule/CMPR.h
@@ -0,0 +1,38 @@
+/*
+ * PowerModule.h
+ *
+ *  Created on: Feb 6, 2015
+ *      Author: TekuConcept
+ */
+
+#ifndef CMPR_H_
+#define CMPR_H_
+
+#include "I2C.h"
+#include "CMPR_Const.h"
+#include "stdint.h"
+
+class PowerModule {
+public:
+	PowerModule(uint _bus_);
+	virtual ~PowerModule();
+
+	// control power to a single ESC
+	void enableESC(uint8_t esc);
+	void disableESC(uint8_t esc);
+
+	// control power to all ESCs
+	void burstEnableESC();
+	void burstDisableESC();
+
+	// values returned range from 0 to 1024
+	uint16_t getAmperageA();
+	uint16_t getAmperageB();
+	uint16_t getAmperageC();
+	uint16_t getAmperageD();
+
+private:
+	I2C bus;
+};
+
+#endif /* CMPR_H_ */
