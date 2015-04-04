1b80dcf0ec3c0d28fe01bf10c75dc68f48c7a92d
TekuConcept <TekuConcept@ymail.com>
2015-04-03 20:43:05 -0600

    Update Environment.h

--- kernel/drivers/Environment.h
+++ kernel/drivers/Environment.h
@@ -1,2 +1,6 @@
+// kernel related options
 #define KNAME 7 // '7'=>wheezy
 #define KVERSION 1 // supported: '1'=>3.8, '2'=>3.14
+
+// build related options
+#define SERVER 0 // supported: '1'=>server enabled
