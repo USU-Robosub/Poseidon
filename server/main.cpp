535c346fd0a59baab145de6d3b792b9756c7f9b6
TekuConcept <TekuConcept@ymail.com>
2015-04-03 21:14:18 -0600

    Update main.cpp

--- server/main.cpp
+++ server/main.cpp
@@ -1,8 +1,30 @@
+#if SERVER != 1
+    #include "rice/Data_Type.hpp"
+    #include "rice/Constructor.hpp"
+#endif
 
 #include "RCServer.hpp"
+#include "../kernel/Environment.h"
 
 int main()
 {
-    RCServer server(4322);
+#if SERVER == 1
+    RCServer server;
+    server.setPort(4322);
     server.start();
+#endif
 }
+
+
+#if SERVER != 1
+extern "C"
+void Init_Server();
+void Init_Server()
+{
+    Rice::Data_Type<RCServer> rb_cRCServer =
+        Rice::define_class<RCServer>("Server")
+        .define_constructor(Rice::Constructor<RCServer>())
+        .define_method("setPort", &RCServer::setPort)
+        .define_method("start", &RCServer::start);
+}
+#endif
