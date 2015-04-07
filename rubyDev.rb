5bdf7031a3c30ff0ed4b7224f0c6df35139b7388
TekuConcept <TekuConcept@ymail.com>
2015-04-06 20:02:04 -0600

    Update rubyDev.rb

--- rubyDev.rb
+++ rubyDev.rb
@@ -16,7 +16,7 @@ module Thrust
 end
 
 # enable ESCs otherwise they won't run
-power = PowerManagement.new
+power = PowerController.new
 power.turnOnESCs()
 
 print "Creating ThrustController instance...\n"
