092c1ca037ad4a3f69a1d80af13633bbad318a7e
Floris Copier <nfloriscopier@coperiansoftworks.com>
2015-11-22 21:38:58 -0700

    Adds the Ruby fork and sets up pipes

--- Main/Peripherals/src/Bootstrap/Bootstrap.cpp
+++ Main/Peripherals/src/Bootstrap/Bootstrap.cpp
@@ -3,6 +3,8 @@
 //
 
 #include <iostream>
+#include <unistd.h>
+#include <sys/wait.h>
 #include <ThrustController.h>
 #include <IMUSensor.h>
 #include <PowerManagement.h>
@@ -11,8 +13,40 @@
 #include "ImuFactory.h"
 #include "PowerModule/CMPR.h"
 
+void setupPipes(int readPipe[2], int writePipe[2]);
+void setupComponents();
+
 int main() {
 
+    // TODO: Replace pipes with shared memory
+    int thrusterPipe[2];
+    int loggerPipe[2];
+    pipe(thrusterPipe);
+    pipe(loggerPipe);
+
+    if(vfork()) {
+        setupPipes(loggerPipe, thrusterPipe);
+        // TODO: Add the name of the actual ruby app
+        execl("ruby", "{ruby_app}");
+    }
+    else {
+        setupComponents();
+        setupPipes(thrusterPipe, loggerPipe);
+        wait(NULL);
+    }
+
+    return 0;
+}
+
+void setupPipes(int readPipe[2], int writePipe[2]) {
+    close(readPipe[1]);
+    dup2(readPipe[0], STDIN_FILENO);
+    close(writePipe[0]);
+    dup2(writePipe[1], STDOUT_FILENO);
+}
+
+
+void setupComponents() {
     auto rubyLogger = std::make_shared<RubyLogger>(&std::cout);
 
     auto thrusterFactory = PwmThrusterFactory();
@@ -23,8 +57,6 @@ int main() {
 
     auto powerModule = std::make_shared<PowerModule>(I2C_SUB2);
     PowerManagement(powerModule, rubyLogger);
-
-    return 0;
 }
 
 
