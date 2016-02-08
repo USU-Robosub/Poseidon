03095e8c3afa870da3b0b3ae61027ed8b443623e
Chris Walker <TekuConcept@ymail.com>
2016-02-07 22:48:36 -0700

    Added Working Driver Files to Project
    All four drivers have been successfully tested and yield valid
    functionality / data.

---
+++ Main/Peripherals/include/Drivers/BMP085.h
@@ -0,0 +1,84 @@
+/**
+  * \class BMP085
+  *
+  *  Created on: Jan 29, 2015
+  * Last Edited: Feb  7, 2016
+  *        Fork: https://github.com/adafruit/Adafruit-BMP085-Library
+  *
+  * \brief This is a hardware wrapper for the BMP085 pressure sensor.
+  *        It provides fundimental sensor readings and control in an orginized manner.
+  */
+
+#ifndef BMP085_H_
+#define BMP085_H_
+
+#include "I2C.h"
+#include "IMU_Const.h"
+#include <chrono>
+#include <thread>
+#include <cmath>
+
+#define delay(time) std::this_thread::sleep_for(std::chrono::milliseconds(time));
+
+class BMP085
+{
+    public:
+        /** \brief Initializes a new pressure sensor for depth and altitude analysis.
+          * \param bus The address of the I2C bus the sensor listens on.
+          */
+        BMP085();
+        virtual ~BMP085();
+
+        /** \brief Initializes the environment and establishes calibration constants used in future computations.
+          * \param mode The mode used to determine the accuracy of measurements.
+          * \return Returns true if initialization was successful.
+          */
+        bool initialize(uint8_t mode = IMU_ENVIRONMENT_STANDARD);
+
+        /** \brief Makes a temperature reading from the sensor.
+          * \return Returns a measurement in °C with an accuracy of ± 0.1.
+          */
+        float readTemperature();
+
+        /** \brief Makes a pressure reading from the sensor.
+          * \return Returns a measurement in Pascals (Pa)
+          */
+        int32_t readPressure();
+
+        /** \brief Makes a pressure reading calculated at sealevel.
+          * \param Optionally provide an altitude in meters describing the sensor's position.
+          * \return Returns a measurement in Pascals (Pa)
+          */
+        int32_t readSealevelPressure(float altitude_meters = 0);
+
+        // read the approximate altitude in meters (m)
+        // params: current provided sea level pressure
+        /** \brief Makes a pressure reading and calculates the current altitude.
+          * \param sealevelPressure The measured sealevel pressure of the environment.
+          * \return Returns the altitude in meters.
+          */
+        float readAltitude(float sealevelPressure = 101325);
+
+    private:
+        // apply compensation
+        int32_t computeB5(int32_t UT);
+
+        // bit swap to correct endianness
+        uint16_t endian16(uint16_t x);
+        uint8_t endian8(uint8_t x);
+
+        // read raw sensor data
+        /** \brief Returns the raw sensor data for temperature.
+          */
+        uint16_t readRawTemperature(void);
+
+        /** \brief Returns the raw sensor data for pressure.
+          */
+        uint32_t readRawPressure(void);
+
+        int16_t ac1, ac2, ac3, b1, b2, mb, mc, md;
+        uint16_t ac4, ac5, ac6;
+        uint8_t oversampling;
+};
+
+#endif /* BMP085_H_ */
---
+++ Main/Peripherals/src/Drivers/MPU6050.cpp
@@ -0,0 +1,83 @@
+/*
+ * MPU6050.cpp
+ *
+ *  Created on: Feb 2, 2015
+ *      Author: TekuConcept
+ */
+
+#include "MPU6050.h"
+
+
+MPU6050::MPU6050() {}
+
+
+
+MPU6050::~MPU6050() {}
+
+
+
+int16_t MPU6050::accel_X() {
+    return I2C::readShort(IMU_MOTION_ADDR_A, IMU_MOTION_ACCEL_XOUT_H);
+}
+
+
+
+int16_t MPU6050::accel_Y() {
+    return I2C::readShort(IMU_MOTION_ADDR_A, IMU_MOTION_ACCEL_YOUT_H);
+}
+
+
+
+int16_t MPU6050::accel_Z() {
+    return I2C::readShort(IMU_MOTION_ADDR_A, IMU_MOTION_ACCEL_ZOUT_H);
+}
+
+
+
+int16_t MPU6050::gyro_X(){
+    return I2C::readShort(IMU_MOTION_ADDR_A, IMU_MOTION_GYRO_XOUT_H);
+}
+
+
+
+int16_t MPU6050::gyro_Y() {
+    return I2C::readShort(IMU_MOTION_ADDR_A, IMU_MOTION_GYRO_YOUT_H);
+}
+
+
+
+int16_t MPU6050::gyro_Z() {
+    return I2C::readShort(IMU_MOTION_ADDR_A, IMU_MOTION_GYRO_ZOUT_H);
+}
+
+
+
+float MPU6050::temperature() {
+    int16_t v = 0;
+    v = I2C::readShort(IMU_MOTION_ADDR_A, IMU_MOTION_TEMP_OUT_H);
+    return (v / 340) + 36.53F;
+}
+
+
+
+void MPU6050::setGyroFullScale(FS_SEL select) {
+    I2C::writeByte(IMU_MOTION_ADDR_A, IMU_MOTION_GYRO_CONFIG, static_cast<int>(select) << 3);
+}
+
+
+
+void MPU6050::setAcclFullScale(AFS_SEL select) {
+    I2C::writeByte(IMU_MOTION_ADDR_A, IMU_MOTION_ACCEL_CONFIG, static_cast<int>(select) << 3);
+}
+
+
+
+void MPU6050::sleep() {
+    I2C::writeByte(IMU_MOTION_ADDR_A, IMU_MOTION_PWR_MGMT_1, 0x40);
+}
+
+
+
+void MPU6050::awake() {
+    I2C::writeByte(IMU_MOTION_ADDR_A, IMU_MOTION_PWR_MGMT_1, 0x00);
+}
