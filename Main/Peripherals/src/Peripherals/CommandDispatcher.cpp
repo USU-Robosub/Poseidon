d0dca8283c737a4d626c0fbdb0ce4bc99ca691aa
Kevin Burgon <kevin.burgon@aggiemail.usu.edu>
2016-04-05 18:21:34 -0600

    Adds imu power commands to commandDispatcher

--- Main/Peripherals/src/Peripherals/CommandDispatcher.cpp
+++ Main/Peripherals/src/Peripherals/CommandDispatcher.cpp
@@ -24,6 +24,8 @@ void CommandDispatcher::dispatchCommand(std::stringstream& cmdString) {
     else if(cmd == "turnOnEscs") powerManager_.turnOnEscs();
     else if(cmd == "turnOffEscs") powerManager_.turnOffEscs();
     else if(cmd == "switchLights") lights_.switchLights();
+    else if(cmd == "turnOnImu") powerManager_.turnOnImuSensor();
+    else if(cmd == "turnOffImu") powerManager_.turnOffImuSensor();
     else if(cmd == "exit") shouldExit_ = true;
 }
 
