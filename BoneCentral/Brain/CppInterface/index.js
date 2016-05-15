3a5013bbef522a80bde55060dcce30d7fa8b822f
N. Floris Copier <nfcopier@gmail.com>
2016-05-14 22:45:02 -0600

    Move child process interface

--- BoneCentral/Brain/CppInterface/index.js
+++ BoneCentral/Brain/CppInterface/index.js
@@ -2,12 +2,6 @@
  * Created by Nathan Copier on 2/12/2016.
  */
 
-var Spawner = require("child_process");
-var Ports = require("../Sockets/Ports.json");
-var Path = require("path");
-
-var childProcess = null;
-
 module.exports = {
     HeadLights: require("./Headlights"),
     PowerManager: require("./PowerManager"),
@@ -15,14 +9,5 @@ module.exports = {
     CppLogSource: require("./CppLogSource"),
     ImuSensor: require("./ImuSensor"),
     Factory: require("./Factory"),
-    initializePeripherals: function () {
-        var args = [
-            "--dispatcherPort=" + Ports.DispatcherPort,
-            "--loggerPort=" + Ports.LoggerPort
-        ];
-        childProcess = Spawner.spawn(Path.resolve(__dirname, "../../Peripherals/Release/Peripherals"), args);
-    },
-    exitPeripherals: function () {
-        // exit child process
-    }
+    Peripherals: require("./PeripheralsProcess")
 };
