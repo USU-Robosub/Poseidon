a88d3ca26ac7b8e5b1f051040a55541ac7c035ab
N. Floris Copier <nfcopier@gmail.com>
2016-05-14 23:05:26 -0600

    Removes some unnecessary debug code

--- BoneCentral/Peripherals/Peripherals.Core/src/CommandDispatcher.cpp
+++ BoneCentral/Peripherals/Peripherals.Core/src/CommandDispatcher.cpp
@@ -3,7 +3,6 @@
 //
 
 #include "CommandDispatcher.h"
-#include <unistd.h>
 using json = nlohmann::json;
 
 CommandDispatcher::CommandDispatcher(std::istream& in, std::ostream& out, ImuSensor& imuSensor, ThrustController& thrustController, PowerManager& powerManager, IHeadlights& lights)
@@ -27,8 +26,6 @@ void CommandDispatcher::runLoop() {
 void CommandDispatcher::dispatchCommand(std::stringstream& cmdString) {
     std::string cmd;
     cmdString >> cmd;
-    std::cout << "Received command: `" << cmd << "` with length: " << cmd.size() << std::endl;
-    sleep(2);
     if(cmd == "goDirection") goDirection(cmdString);
     else if(cmd == "faceDirection") faceDirection(cmdString);
     else if(cmd == "turnOnEscs") powerManager_.turnOnEscs();
@@ -99,7 +96,6 @@ void CommandDispatcher::_getAngularAcceleration() {
             {"Y", std::get<1>(data)},
             {"Z", std::get<2>(data)}
     };
-    std::cout << accelJson << std::endl;
     out_ << accelJson << std::endl;
 }
 
