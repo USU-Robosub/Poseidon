02749a4ef23f8ca52dae34e26ee2e5820a025d32
N. Floris Copier <nfcopier@gmail.com>
2016-05-23 23:19:47 -0600

    Adds Clockwise/ConterClockwise scripts

--- BoneCentral/WebApplication/TestScripts/TurnCounterClockwise.js
+++ BoneCentral/WebApplication/TestScripts/TurnCounterClockwise.js
@@ -3,13 +3,13 @@
  */
 
 var Utilities = require("../../Brain/Utilities");
+var timeDelay = 1000;
 
 module.exports = (function () {
     return {
         execute: function (thrustController, imuSensor, logger) {
-            var timeDelay = 1000;
             logger.info("Turning for " + timeDelay + " milliseconds.");
-            thrustController.faceDirection(0.2, 0);
+            thrustController.faceDirection(-0.2, 0);
             var promise = Utilities.Wait(timeDelay);
             promise.then(function () {
                 thrustController.faceDirection(0, 0);
