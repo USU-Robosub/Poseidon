1eb3ca6f62ea7a813b9ea7360aa4651f5fb9cc68
N. Floris Copier <nfcopier@gmail.com>
2016-05-26 16:09:22 -0600

    Adds Vision interface with GateDetector

---
+++ BoneCentral/Brain/VisionInterface/GateDetector.js
@@ -0,0 +1,38 @@
+/**
+ * Created by Nathan Copier on 5/26/2016.
+ */
+
+var Promises = require("../Utilities").Promises;
+
+module.exports = (function () {
+
+    function GateDetector(iStream, oStream, logger) {
+        this._oStream = oStream;
+        this._logger = logger;
+        _initializePromises.call(this);
+        iStream.on("data",  _handleData(this));
+    }
+
+    var _initializePromises = function () {
+        this._poleCoordRequest = Promises.Deferred().resolve();
+    };
+
+    var _handleData = function (self) { return function (data) {
+        try {
+            var dataJson = JSON.parse(data.toString());
+            if(dataJson.Type == "PoleCoordinates") self._poleCoordRequest.resolve(dataJson);
+        }
+        catch (e) {}
+    };};
+
+    GateDetector.prototype.GetPoleCoordinates = function () {
+        if(this._poleCoordRequest.state() !== "pending") {
+            this._poleCoordRequest = Promises.Deferred();
+            this._oStream.write("GATE_allX\n");
+        }
+        return this._poleCoordRequest.promise();
+    };
+
+    return GateDetector;
+
+})();
