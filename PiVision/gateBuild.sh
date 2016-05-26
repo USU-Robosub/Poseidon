5730be3606184528747d2f991c4128fc102f4561
Kevin Burgon <kevin.burgon@aggiemail.usu.edu>
2016-05-26 04:26:26 +0000

    Adds build script to PiVision

--- PiVision/gateBuild.sh
+++ PiVision/gateBuild.sh
@@ -1,2 +1,7 @@
-g++ -c Capture.cpp GateDetector.cpp `pkg-config --cflags opencv` -std=c++11
-g++ gateDetectorMain.cpp Capture.o GateDetector.o -o gateDetect `pkg-config --cflags opencv pkg-config --libs opencv` -std=c++11
+if [ -d Build ]; then
+	rm -rf Build
+fi
+mkdir Build
+cd Build
+cmake ..
+make
