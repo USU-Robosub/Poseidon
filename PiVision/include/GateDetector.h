5151034214b6fed83d2ed7d4b74b821b509c1fd4
N. Floris Copier <nfcopier@gmail.com>
2016-06-24 00:54:42 -0600

    Removes line algorithm; Stores poles as JSON

--- PiVision/include/GateDetector.h
+++ PiVision/include/GateDetector.h
@@ -19,13 +19,6 @@ public:
     void setHsvValues(json hsvJson);
 
 private:
-    void clusterPoles(std::vector<int> lineXCoords);
-    int averageLines(std::vector<int> lineXCoords);
-    std::vector<int> lineXCoords;
-    const int ANGLE = 25;
-    const int MAX_DIST = 50;
-    float averageXCoord = 0;
-
     // Used for finding the orange color, subject to change
     void readHsvValues_(json hsvJson);
     int _lowHue = 0;
@@ -35,6 +28,5 @@ private:
     int _lowValue = 0;
     int _highValue = 255;
 
-    int rightGateX = -1;
-    int leftGateX = -1;
+    json poles_ = json::array();
 };
--- PiVision/src/GateDetector.cpp
+++ PiVision/src/GateDetector.cpp
@@ -8,54 +8,6 @@ GateDetector::GateDetector() {
     refreshHsv();
 }
 
-int GateDetector::averageLines(std::vector<int> lineXCoords)
-{
-    float totalX = 0;
-    for( size_t i = 0; i < lineXCoords.size(); i++ )    
-    {
-        totalX += lineXCoords[i];
-    }
-
-    return int(totalX / lineXCoords.size());
-}
-
-void GateDetector::clusterPoles(std::vector<int> lineXCoords)
-{
-    bool left = true;
-    std::vector<int> xPos;
-    std::sort(lineXCoords.begin(), lineXCoords.end());
-
-    if(lineXCoords.size() > 1)
-    {
-        xPos.push_back(lineXCoords[0]);
-        for(size_t i = 1; i < lineXCoords.size(); i++)
-        {
-            if(lineXCoords[i] - xPos[i - 1] > MAX_DIST || i == lineXCoords.size() - 1)
-            {
-                if(left)
-                {
-                    leftGateX = averageLines(xPos);
-                    left = false;
-                    xPos.clear();
-                }
-                else
-                {
-                    rightGateX = averageLines(xPos);
-                }
-            }
-
-            xPos.push_back(lineXCoords[i]);
-        }
-    }
-    else
-    {
-        leftGateX = -1;
-        rightGateX = -1;
-    }
-
-    averageXCoord = averageLines(lineXCoords);
-}
-
 void GateDetector::process(cv::Mat img)
 {
     cv::Mat grayImg = Capture::grayscale(img);
@@ -73,7 +25,6 @@ void GateDetector::process(cv::Mat img)
     cv::erode(thresholdedImg, thresholdedImg, cv::getStructuringElement(cv::MORPH_ELLIPSE, cv::Size(5, 5)) );
 
     cv::Mat edgesImg, cdst;
-    std::vector<cv::Vec4i> lines;
 
     // Detect edges
     cv::Canny(thresholdedImg, edgesImg, 50, 200, 3);
@@ -128,43 +79,21 @@ void GateDetector::process(cv::Mat img)
     }
 
     std::vector<cv::RotatedRect> poles;
+    poles_.clear();
     for (auto n = 0u; n < rects.size(); n++) {
         if (rects[n].size.area()*3 < largestArea) continue;
         poles.push_back(rects[n]);
         cv::Point2f points[4]; rects[n].points(points);
+        json pointsJson;
+        for (auto o = 0u; o < 4; o++) {
+            json pointJson = {{"X", points[o].x}, {"Y", points[o].y}};
+            pointsJson.push_back(pointJson);
+        }
+        poles_.push_back(pointsJson);
         cv::line(cdst, points[0], points[1], cv::Scalar(255,0,0));
         cv::line(cdst, points[1], points[2], cv::Scalar(255,0,0));
         cv::line(cdst, points[2], points[3], cv::Scalar(255,0,0));
         cv::line(cdst, points[3], points[0], cv::Scalar(255,0,0));
-    }
-
-    std::cout << "Pole Count: " << poles.size() << std::endl;
-
-    // Find all lines of a certain length
-    cv::HoughLinesP(edgesImg, lines, 1, CV_PI/180, 50, 100, 10);
-    if(lines.size() > 0)
-    {
-        lineXCoords.clear();
-        // Iterate through lines
-        for( size_t i = 0; i < lines.size(); i++ )
-        {
-            cv::Vec4i line = lines[i];
-            // Make sure the line isn't horizontal
-            if(std::abs(line[0] - line[2]) <= ANGLE)
-            {
-                lineXCoords.push_back(convertXCoordinate(line[0]));
-                lineXCoords.push_back(convertXCoordinate(line[2]));
-                /*
-                // Debugging to give position and draw lines on the detected gates
-                std::cout << "WIDTH: " << frameWidth << " HEIGHT: " << frameHeight << std::endl;
-                std::cout << "x1: " << convertXCoordinate(line[0]) << " y1: " << convertYCoordinate(line[1]) << std::endl;
-                std::cout << "x2: " << convertXCoordinate(line[2]) << " y2: " << convertYCoordinate(line[3]) << std::endl;
-                cv::line( cdst, cv::Point(line[0], line[1]), cv::Point(line[2], line[3]), cv::Scalar(0,0,255), 3, CV_AA);
-                */
-            }
-        }
-
-        clusterPoles(lineXCoords);
     }
 
     // If you want to save images
@@ -172,7 +101,7 @@ void GateDetector::process(cv::Mat img)
     //cv::imwrite("lines.jpg", cdst);
 
     // If you want to show the image on screen (debugging only)
-    cv::imshow("lines", cdst);
+    cv::imshow("poles", cdst);
 
     // Must stay in if you are debugging
     cv::waitKey(30);
@@ -193,25 +122,9 @@ void GateDetector::handleInput(std::string command)
 {
     Capture::handleInput(command);
 
-    if(command.compare("GATE_center") == 0)
+    if(command.compare("getPoleCount") == 0)
     {
-        std::cout << averageXCoord << std::endl;
-    }
-    else if(command.compare("GATE_right") == 0)
-    {
-        std::cout << rightGateX << std::endl; // give you the avg right gate position
-    }
-    else if(command.compare("GATE_left") == 0)
-    {
-        std::cout << leftGateX << std::endl; // give the avg left gate position
-    }
-    else if(command.compare("GATE_allX") == 0)
-    {
-        // here you could send the line x coordinates through the socket
-        std::cout << lineXCoords.size() << std::endl;
-    }
-    else if(command.compare("RefreshHsv") == 0) {
-        refreshHsv();
+        std::cout << poles_.size() << std::endl;
     }
 }
 
