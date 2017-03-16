386788d742072bb9db1e17e2d7d4508791528c97
TekuConcept <electro_madness@yahoo.com>
2017-03-15 19:21:16 -0600

    Web App Clean Up and Styling

--- BoneCentral/WebApplication/index.js
+++ BoneCentral/WebApplication/index.js
@@ -1,34 +1,31 @@
-var express = require('express');
-var bodyParser = require('body-parser');
-var fileSystem = require('fs');
-var path = require("path");
-var CppInterface = require('../Brain/CppInterface');
-var VisionInterface = require("../Brain/VisionInterface");
-var WebLogger = require('./WebLogger');
-var FileLogger = require('./FileLogger');
-var app = express();
-var GoThroughGate = require("../Brain/GoThroughGate");
+var express         	= require('express');
+var bodyParser      	= require('body-parser');
+var fileSystem      	= require('fs');
+var path        		= require("path");
+var WebLogger       	= require('./WebLogger');
+var FileLogger      	= require('./FileLogger');
+var CppInterface		= require('../Brain/CppInterface');
+var VisionInterface		= require("../Brain/VisionInterface");
+var GoThroughGate   	= require("../Brain/GoThroughGate");
+var ThrustManager   	= require("../Brain/ThrustManager");
 
-var peripheralsFactory = new CppInterface.Factory();
-var ThrustManager = require("../Brain/ThrustManager");
-var thrustManager = new ThrustManager(peripheralsFactory);
+var fileLogger 		  	= new FileLogger("./test.log");
+var webLogger 			= new WebLogger(fileLogger);
+var peripheralsFactory	= new CppInterface.Factory();
+var visionFactoy 	  	= new VisionInterface.Factory();
+var thrustManager    	= new ThrustManager(peripheralsFactory);
+var goThroughGate 		= new GoThroughGate(visionFactoy, thrustManager, webLogger);
 
-var thrustController = peripheralsFactory.createThrustController();
-var headLights = peripheralsFactory.createHeadlights();
-var powerManager = peripheralsFactory.createPowerManager();
-var imuSensor = peripheralsFactory.createImuSensor();
+var thrustController    = peripheralsFactory.createThrustController();
+var powerManager 		= peripheralsFactory.createPowerManager();
+var imuSensor 			= peripheralsFactory.createImuSensor();
+var headLights 			= peripheralsFactory.createHeadlights();
+var gateDetector 		= visionFactoy.createGateDetector(webLogger);
 
-var fileLogger = new FileLogger("./test.log");
-var webLogger = new WebLogger(fileLogger);
 peripheralsFactory.createCppLogSource(webLogger);
-
 CppInterface.Peripherals.initialize();
 
-var visionFactoy = new VisionInterface.Factory();
-var gateDetector = visionFactoy.createGateDetector(webLogger);
-
-var goThroughGate = new GoThroughGate(visionFactoy, thrustManager, webLogger);
-
+app             	= express();
 app.use('/', express.static('static'));
 app.use(bodyParser.json());
 
@@ -243,7 +240,6 @@ app.post('/runScript', function(req, res) {
 	})
 });
 
-
 app.listen(80, function () {
-  console.log('Example app listening on port 80!');
+  console.log('Web app listening on port 80!');
 });
