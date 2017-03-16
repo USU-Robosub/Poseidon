0838e0689926b1796df53d105b4f0ba76706133f
TekuConcept <electro_madness@yahoo.com>
2017-03-15 19:00:30 -0600

    Serial Communication
    Adds serial communication and debugging between microcontrollers

---
+++ ArduinoController/PinMap.h
@@ -0,0 +1,28 @@
+#ifndef PIN_MAP_H
+#define PIN_MAP_H
+
+#define KILLSWITCH_PIN  50
+
+#define LED_STAT_PIN    22
+#define LED_CTRL_PIN    52
+
+#define LIGHTS_PIN      45
+
+#define MOVE_PIN        13
+#define STRAFE_PIN      12
+#define DIVE_PIN        11
+
+#define YAW_PIN         9
+#define PITCH_PIN       8
+#define ROLL_PIN        7
+
+#define ESC_S1_PIN      36
+#define ESC_S2_PIN      39
+#define ESC_S3_PIN      41
+#define ESC_S4_PIN      47
+#define ESC_S5_PIN      49
+#define ESC_S6_PIN      53
+
+#define VOLT_PIN        10
+
+#endif
