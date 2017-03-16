64f8dd16f69234b89767577ff7d7bebdf4371131
TekuConcept <electro_madness@yahoo.com>
2017-03-16 19:12:12 +0000

    Moved Hardware Commands to Seperate File

--- BoneCentral/WebApplication/index.js
+++ BoneCentral/WebApplication/index.js
@@ -1,36 +1,14 @@
 var express         	= require('express');
 var bodyParser      	= require('body-parser');
-var fileSystem      	= require('fs');
-var path        		= require("path");
 var routes              = require('./routes.js');
-var WebLogger       	= require('./WebLogger');
-var FileLogger      	= require('./FileLogger');
-var CppInterface		= require('../Brain/CppInterface');
-var VisionInterface		= require("../Brain/VisionInterface");
-var GoThroughGate   	= require("../Brain/GoThroughGate");
-var ThrustManager   	= require("../Brain/ThrustManager");
+var messenger           = require('./messenger.js');
+var port                = 80;
+var app             	= express();
 
-var fileLogger 		  	= new FileLogger("./test.log");
-var webLogger 			= new WebLogger(fileLogger);
-var peripheralsFactory	= new CppInterface.Factory();
-var visionFactoy 	  	= new VisionInterface.Factory();
-var thrustManager    	= new ThrustManager(peripheralsFactory);
-var goThroughGate 		= new GoThroughGate(visionFactoy, thrustManager, webLogger);
-
-var thrustController    = peripheralsFactory.createThrustController();
-var powerManager 		= peripheralsFactory.createPowerManager();
-var imuSensor 			= peripheralsFactory.createImuSensor();
-var headLights 			= peripheralsFactory.createHeadlights();
-var gateDetector 		= visionFactoy.createGateDetector(webLogger);
-
-peripheralsFactory.createCppLogSource(webLogger);
-CppInterface.Peripherals.initialize();
-
-app             	= express();
 app.use('/', express.static('static'));
 app.use(bodyParser.json());
-routes(app);
+routes(app, messenger);
 
-app.listen(80, function () {
+app.listen(port, function () {
   console.log('Web app listening on port 80!');
 });
