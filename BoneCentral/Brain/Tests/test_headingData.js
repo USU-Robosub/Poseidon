0b8b6d49765dd361d3941449761575ab40f4825c
TekuConcept <electro_madness@yahoo.com>
2017-03-18 02:00:39 +0000

    Added Some Brains Tests

---
+++ BoneCentral/Brain/Tests/test_headingData.js
@@ -0,0 +1,39 @@
+var $           = require('../Utilities').Promises
+var wait        = require('../Utilities').Wait;
+var Poseidon    = require('../app.js');
+
+var power       = Poseidon.HardwareFactory.createPowerManager();
+var imu         = Poseidon.HardwareFactory.createImuSensor();
+
+
+var test = $.Deferred();
+
+test
+.then(wait(5000))
+.then(function() {
+    console.log("IMU On");
+    power.turnOnImu();
+})
+.then(wait(250))
+.then(function() {
+    console.log("Fetch 1");
+    imu.getHeading().then(function(heading){
+        console.log(heading);
+    });
+})
+.then(wait(250))
+.then(function() {
+    console.log("Fetch 2");
+    imu.getHeading().then(function(heading){
+        console.log(heading);
+    });
+})
+.then(wait(250))
+.done(function() {
+    console.log("IMU Off");
+    power.turnOffImu();
+    power.exit();
+    process.exit();
+});
+
+test.resolve();
