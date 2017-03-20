2491b65c28255fdff0f130d3ca7711a10d178205
TekuConcept <electro_madness@yahoo.com>
2017-03-20 21:34:33 +0000

    Added Hardware Shutdown Promise

--- BoneCentral/Brain/CppInterface/Factory.js
+++ BoneCentral/Brain/CppInterface/Factory.js
@@ -22,7 +22,7 @@ module.exports = (function() {
     };
 
     Factory.prototype.createPowerManager = function () {
-        return new PowerManager(this.dispatcherSocket.Input);
+        return new PowerManager(this.dispatcherSocket.Input, this.dispatcherSocket.OnExit);
     };
 
     Factory.prototype.createThrustController = function () {
