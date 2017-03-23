d523bf7a02c2d46817b293ebdbf821b56ae3e9fe
TekuConcept <electro_madness@yahoo.com>
2017-03-23 19:45:36 +0000

    Reorganized AlignToScript

--- BoneCentral/Brain/AI/AlignToAngle.js
+++ BoneCentral/Brain/AI/AlignToAngle.js
@@ -12,8 +12,6 @@ var pid         = new PID();
 var imu         = Poseidon.HardwareFactory.createImuSensor();
 var power       = Poseidon.HardwareFactory.createPowerManager();
 var engine      = Poseidon.HardwareFactory.createThrustController();
-var start       = $.Deferred();
-var finish      = $.Deferred();
 const DELTA     = 150;
 const TRANSFORM = 180.0;
 
@@ -23,33 +21,27 @@ pid.calibrate(0.1, 0.5, 0.01);
 
 
 
-start
-.then(function() {
+power.connect("begin", function(){
+    console.log("Ready? GO!");
+    start();
+});
+
+function start() {
     console.log("- System Online -");
     power.turnOnEscs();
     power.turnOnImu();
-})
-.then(function() {
     upInterval = setInterval(function(){
         update();
         killOutOfTime();
     }, DELTA);
-});
+}
 
-finish
-.then(function() {
+function finish() {
     console.log("- Shutting Down -");
     power.turnOffImu();
     power.turnOffEscs();
-    power.exit()
-    .then(process.exit);
-})
-
-power.connect()
-.then(function(){
-    console.log("Ready? GO!");
-    start.resolve();
-});
+    power.exit("end", process.exit);
+}
 
 
 
@@ -66,15 +58,15 @@ function update() {
     
     $.WhenAll(waitHeading, waitAccel)
     .done(function(cdata,adata) {
-        // console.log(cdata);
-        // console.log(adata);
-    //     var compass = vector.v3(cData.X,cData.Y,cData.Z);
-    //     var accel   = vector.v3(aData.X,aData.Y,aData.Z);
+        console.log(cdata);
+        console.log(adata);
+        var compass = vector.v3(cData.X,cData.Y,cData.Z);
+        var accel   = vector.v3(aData.X,aData.Y,aData.Z);
         
-    //     var pid_pv  = ctools.degrees(compass, accel);
-    //     var adj     = pid.update(pid_sp, pid_pv) / TRANSFORM;
-    //     console.log("Adjust: " + (adj*100).toFixed(2) + "%");
-    //     engine.yaw(adj);
+        var pid_pv  = ctools.degrees(compass, accel);
+        var adj     = pid.update(pid_sp, pid_pv) / TRANSFORM;
+        console.log("Adjust: " + (adj*100).toFixed(2) + "%");
+        engine.yaw(adj);
     });
 }
 
@@ -82,6 +74,6 @@ function killOutOfTime() {
     time = time - (DELTA/1000.0);
     if(time <= 0) {
         clearInterval(upInterval);
-        finish.resolve();
+        finish();
     }
 }
