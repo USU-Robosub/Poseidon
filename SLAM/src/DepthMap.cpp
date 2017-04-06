65111c13d7666e6b7fda6d7d494a2f6323f4338f
Kyler Jensen <Kjens93@users.noreply.github.com>
2017-04-06 07:55:09 -0600

    Update DepthMap.cpp
    Minor code formatting and cleanup.

--- SLAM/src/DepthMap.cpp
+++ SLAM/src/DepthMap.cpp
@@ -1,24 +1,21 @@
 #include "../include/telemetry/DepthMap.h"
 
-slam::DepthMap:: DepthMap(unsigned short x, unsigned short y)
-{
-    for(int i = 0; i < x; i++)
-    {
-        std::vector <float> temp;
-        for(int k = 0; k < y; k++)
-        {
+using namespace slam;
+
+DepthMap::DepthMap(unsigned short x, unsigned short y) {
+    for(int i = 0; i < x; i++) {
+        std::vector<float> temp;
+        for(int k = 0; k < y; k++) {
             temp.push_back(UNKNOWN_DEPTH);
         }
         m_depth.push_back(temp);
     }
 }
 
-void slam::DepthMap:: setDepth(unsigned short x, unsigned short y, float depth)
-{
+void DepthMap::setDepth(unsigned short x, unsigned short y, float depth) {
     m_depth[x][y] = depth;
 }
 
-double slam::DepthMap:: getDepth(unsigned short x, unsigned short y)
-{
+double DepthMap::getDepth(unsigned short x, unsigned short y) {
    return m_depth[x][y];
 }
