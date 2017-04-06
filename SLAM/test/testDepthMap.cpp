2b9dae190b3a3a51a434b58706111bfeedb54480
Kyler Jensen <Kjens93@users.noreply.github.com>
2017-04-06 07:55:41 -0600

    Update testDepthMap.cpp
    Minor code formatting.

--- SLAM/test/testDepthMap.cpp
+++ SLAM/test/testDepthMap.cpp
@@ -10,17 +10,17 @@
 
 using namespace slam;
 
-TEST_CASE("DepthMap works as expected"){
+TEST_CASE("DepthMap works as expected") {
     auto test1 = DepthMap(2, 4);
     
-    SECTION("The DepthMap constructor works as expected"){
+    SECTION("The DepthMap constructor works as expected") {
         REQUIRE(test1.getDepth(0, 0) == UNKNOWN_DEPTH);
         REQUIRE(test1.getDepth(0, 3) == UNKNOWN_DEPTH);
         REQUIRE(test1.getDepth(1, 0) == UNKNOWN_DEPTH);
         REQUIRE(test1.getDepth(1, 3) == UNKNOWN_DEPTH);
     }
     
-    SECTION("getDepth() and setDepth() work as expected"){
+    SECTION("getDepth() and setDepth() work as expected") {
         test1.setDepth(0, 3, 2);
         REQUIRE(test1.getDepth(0, 3) == 2);
         
