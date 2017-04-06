296b99201a2cddfa010f518e688cd227f29dae84
Kyler Jensen <Kjens93@users.noreply.github.com>
2017-04-06 07:51:08 -0600

    Update testDepthMap.cpp
    Changed test section descriptions to match existing test section grammar.

--- SLAM/test/testDepthMap.cpp
+++ SLAM/test/testDepthMap.cpp
@@ -10,17 +10,17 @@
 
 using namespace slam;
 
-TEST_CASE("Struct for depth Map test"){
+TEST_CASE("DepthMap works as expected"){
     auto test1 = DepthMap(2, 4);
     
-    SECTION ("Construter Test"){
+    SECTION("The DepthMap constructor works as expected"){
         REQUIRE(test1.getDepth(0, 0) == UNKNOWN_DEPTH);
         REQUIRE(test1.getDepth(0, 3) == UNKNOWN_DEPTH);
         REQUIRE(test1.getDepth(1, 0) == UNKNOWN_DEPTH);
         REQUIRE(test1.getDepth(1, 3) == UNKNOWN_DEPTH);
     }
     
-    SECTION("Test GetDepth and SetDepth"){
+    SECTION("getDepth() and setDepth() work as expected"){
         test1.setDepth(0, 3, 2);
         REQUIRE(test1.getDepth(0, 3) == 2);
         
