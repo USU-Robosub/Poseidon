56a8921605700810880bf174588b4b60c0032205
TekuConcept <electro_madness@yahoo.com>
2017-04-09 23:52:54 +0000

    Moved Voltage Controller Into IMU Controller

--- BoneCentral/Brain/Tests/test_voltage.js
+++ BoneCentral/Brain/Tests/test_voltage.js
@@ -1,7 +1,7 @@
 var Poseidon    = require('../app.js');
 
 var power       = Poseidon.HardwareFactory.createPowerManager();
-var sensors     = Poseidon.HardwareFactory.createAuxiliarySensors();
+var sensors     = Poseidon.HardwareFactory.createImuSensor();
 
 power.connect("run", function() {
     sensors.getVoltage().done(function(data){
