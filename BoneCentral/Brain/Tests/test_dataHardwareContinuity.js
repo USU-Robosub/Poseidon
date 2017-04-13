337424745f8db48107be50870df34c342bd52bb7
TekuConcept <electro_madness@yahoo.com>
2017-04-13 05:35:38 +0000

    Simple Thruster Integrity Test

---
+++ BoneCentral/Brain/Tests/test_dataHardwareContinuity.js
@@ -0,0 +1,47 @@
+var $           = require('../Utilities').Promises
+var wait        = require('../Utilities').Wait;
+var Poseidon    = require('../app.js');
+
+var power       = Poseidon.HardwareFactory.createPowerManager();
+var engine      = Poseidon.HardwareFactory.createThrustController();
+var run         = $.Deferred();
+const THRUST    = 0.3;
+
+power.connect("begin", function(){
+    console.log("- BEGIN TEST -");
+    power.turnOnEscs();
+    run.resolve();
+});
+
+run
+.then(function(){engine.move(          THRUST);}).then(wait(1000))
+.then(function(){engine.move(               0);}).then(wait(1000))
+// .then(function(){engine.move(         -THRUST);}).then(wait(1000))
+// .then(function(){engine.move(               0);}).then(wait(1000))
+.then(function(){engine.secondaryDive( THRUST);}).then(wait(1000))
+.then(function(){engine.secondaryDive(      0);}).then(wait(1000))
+// .then(function(){engine.secondaryDive(-THRUST);}).then(wait(1000))
+// .then(function(){engine.secondaryDive(      0);}).then(wait(1000))
+.then(function(){engine.dive(          THRUST);}).then(wait(1000))
+.then(function(){engine.dive(               0);}).then(wait(1000))
+// .then(function(){engine.dive(         -THRUST);}).then(wait(1000))
+// .then(function(){engine.dive(               0);}).then(wait(1000))
+.then(function(){engine.yaw(           THRUST);}).then(wait(1000))
+.then(function(){engine.yaw(                0);}).then(wait(1000))
+// .then(function(){engine.yaw(          -THRUST);}).then(wait(1000))
+// .then(function(){engine.yaw(                0);}).then(wait(1000))
+.then(function(){engine.pitch(         THRUST);}).then(wait(1000))
+.then(function(){engine.pitch(              0);}).then(wait(1000))
+// .then(function(){engine.pitch(        -THRUST);}).then(wait(1000))
+// .then(function(){engine.pitch(              0);}).then(wait(1000))
+.then(function(){engine.roll(          THRUST);}).then(wait(1000))
+.then(function(){engine.roll(               0);}).then(wait(1000))
+// .then(function(){engine.roll(         -THRUST);}).then(wait(1000))
+// .then(function(){engine.roll(               0);}).then(wait(1000))
+.then(wait(1000))
+.done(function(){
+    console.log("- END TEST -");
+    engine.kill();
+    power.turnOffEscs();
+    power.exit("end", process.exit);
+});
