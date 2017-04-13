1367663f862a1a27825780d68dafd3339d1a3d36
TekuConcept <electro_madness@yahoo.com>
2017-04-13 14:28:37 -0600

    StartController now writes to the event serial object
    Serial1 is researved for debugging and Serial is the main line of communication, so, Serial2 was selected as the event line.

--- ArduinoController/StartController.h
+++ ArduinoController/StartController.h
@@ -2,16 +2,21 @@
 
 class StartController : public IController {
 private:
-  bool activated = false;
+  bool pending = false;
+  uint8_t value = 0;
   const int ACTIVE   = 0x1;
   const int INACTIVE = 0x0;
   
 public:
+  bool isPending() {
+    return pending;
+  }
   void execute() {
-    SerialTools::writeByte(activated?ACTIVE:INACTIVE);
-    activated = false;
+    SerialTools::writeByte(value, EVENT);
+    pending = false;
   }
   void kill() {
-    activated = true;
+    value = digitalRead(GpioPin::KILLSWITCH_PIN);
+    pending = true;
   }
 };
