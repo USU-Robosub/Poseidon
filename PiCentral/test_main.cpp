faac80b50136d6b61e4113f6c81f18cea67cbf49
Cameron Milne <cameronm213@gmail.com>
2017-10-07 13:47:13 -0600

    Added tacopie library

--- PiCentral/test_main.cpp
+++ PiCentral/test_main.cpp
@@ -2,9 +2,14 @@
 #include "gmock/gmock.h"
 
 #include <opencv2/opencv.hpp>
+#include <tacopie/tacopie>
 
 TEST(PiVision, IsIncluded) {
 	cv::Mat frame;
+}
+
+TEST(TacoPie, IsIncluded) {
+	tacopie::tcp_client client;
 }
 
 int main(int argc, char** argv){
