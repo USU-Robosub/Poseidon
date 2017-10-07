2c145e3e970e401c211e6ca77da477589a3cfc34
Cameron Milne <cameronm213@gmail.com>
2017-10-07 14:53:39 -0600

    Added vision driver

--- PiCentral/src/main.cpp
+++ PiCentral/src/main.cpp
@@ -1,3 +1,24 @@
-int main() {
+#include <string>
+#include <cstdint>
+#include <iostream>
+#include <tacopie/tacopie>
+
+int main(int argc, char** argv) {
+	
+	if(argc != 6) {
+		std::cout << "Invalid number of arguments to " << argv[0] << std::endl;
+		std::cout << "Usage: <address> <port> <camera_file_path> <camera_number> <template_image_url>" << std::endl;
+		return 1;
+	}
+	
+	std::string address = argv[1];
+	std::uint32_t port = std::stoi(argv[2]);
+	std::string camera_file_path = argv[3];
+	std::uint32_t camera_number = std::stoi(argv[4]);
+	std::string template_image_url = argv[5];
+	
+	tacopie::tcp_client client;
+	client.connect(address, port);
+	
     return 0;
 }
